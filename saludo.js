d2bd52ae78d3f31dbbcaaa3b593cf3d9e8ca3b3e
Layonar <alayonar@gmail.com>
2022-11-08 13:11:24 -0500

    nuevo cambio 2

--- saludo.js
+++ saludo.js
@@ -1,3 +1,5 @@
 alert("Hola");
 let nombre = prompt("Escriba su nombre");
 document.write("Su nombre es: "+nombre);
+let apellido = prompt("Escriba su primer apellido");
+document.write("Su nombre es: "+apellido);
